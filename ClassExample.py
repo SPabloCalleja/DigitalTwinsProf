f767dae24f3695fe470a8483a2b68b76019bb251
SerPablo <p.calleja@upm.es>
2024-10-29 20:23:35 +0100

    New file

---
+++ ClassExample.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 29 19:19:11 2024
+
+@author: pablo
+"""
+
+
+variable= 5
+
+
+
+variable = variable + 10
+
+print(variable)
+
+my_list=[ ['hello','world'],[ 'this','is'  ],['a','example']   ]
+
+
+
